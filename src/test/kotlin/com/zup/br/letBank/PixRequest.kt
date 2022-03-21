eedb069d0ce78e861611aa36a16c1378e810145d
Let Marçal <leticiamarcalmatias@gmail.com>
2022-03-21 14:37:21 -0300

    include validations in PixRequest

--- src/test/kotlin/com/zup/br/letBank/PixRequest.kt
+++ src/test/kotlin/com/zup/br/letBank/PixRequest.kt
@@ -1,8 +1,10 @@
 package com.zup.br.letBank
 
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
 
 
 data class PixRequest (
-    val email: String,
-    val keyPix: String
+    @field:[NotBlank Email] val email: String,
+    @field:[NotBlank] val keyPix: String
         )
