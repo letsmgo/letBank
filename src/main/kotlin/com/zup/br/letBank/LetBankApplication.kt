5ef248344438fa8dcf763c2b63edf055b71e32d4
Let Marçal <leticiamarcalmatias@gmail.com>
2022-03-21 14:22:30 -0300

    Initial commit

---
+++ src/main/kotlin/com/zup/br/letBank/LetBankApplication.kt
@@ -0,0 +1,11 @@
+package com.zup.br.letBank
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LetBankApplication
+
+fun main(args: Array<String>) {
+	runApplication<LetBankApplication>(*args)
+}
