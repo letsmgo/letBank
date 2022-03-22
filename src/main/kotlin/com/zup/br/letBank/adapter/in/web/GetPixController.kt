e6513532ff82b9006e5bb0e5e175b4686e5ad90e
Let Marcal <leticiamarcalmatias@gmail.com>
2022-03-22 11:38:52 -0300

    get method in controller

--- src/main/kotlin/com/zup/br/letBank/adapter/in/web/GetPixController.kt
+++ src/main/kotlin/com/zup/br/letBank/adapter/in/web/GetPixController.kt
@@ -1,4 +1,17 @@
 package com.zup.br.letBank.adapter.`in`.web
 
+import com.zup.br.letBank.adapter.`in`.web.request.PixRequest
+import com.zup.br.letBank.adapter.`in`.web.response.PixResponse
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
+
+@RestController
 class GetPixController {
+
+    @GetMapping("v1/pix")
+    fun getPix(@RequestBody @Valid pixRequest: PixRequest): PixResponse {
+        return pixRequest.mapperToPixResponse(email = pixRequest.email, keyPix = pixRequest.keyPix)
+    }
 }
