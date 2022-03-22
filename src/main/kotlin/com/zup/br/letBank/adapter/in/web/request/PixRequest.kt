4dd205a5ba4aa143a468d89421d97b64cb9847a1
Let Marcal <leticiamarcalmatias@gmail.com>
2022-03-22 11:38:21 -0300

    add mapperToPixResponse

--- src/main/kotlin/com/zup/br/letBank/adapter/in/web/request/PixRequest.kt
+++ src/main/kotlin/com/zup/br/letBank/adapter/in/web/request/PixRequest.kt
@@ -1,5 +1,6 @@
 package com.zup.br.letBank.adapter.`in`.web.request
 
+import com.zup.br.letBank.adapter.`in`.web.response.PixResponse
 import javax.validation.constraints.Email
 import javax.validation.constraints.NotBlank
 
@@ -7,4 +8,11 @@ import javax.validation.constraints.NotBlank
 data class PixRequest (
     @field:[NotBlank Email] val email: String,
     @field:[NotBlank] val keyPix: String
-        )
+    ){
+        fun mapperToPixResponse(email: String, keyPix: String) : PixResponse =
+                PixResponse(
+                        keyPixResponse = keyPix,
+                        email = email
+                )
+
+}
